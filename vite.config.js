e9c39bb7891812ba678f99db4fc9b41aef2090d5
Harshaditya Sharma <106009563+hercules2209@users.noreply.github.com>
2024-10-09 14:32:38 +0530

    minor build setting changes

--- vite.config.js
+++ vite.config.js
@@ -6,30 +6,6 @@ export default defineConfig({
     react(),
   ],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'react-vendors';
-            }
-            if (id.includes('firebase')) {
-              return 'firebase-chunk';
-            }
-            if (id.includes('@reduxjs/toolkit') || id.includes('react-redux')) {
-              return 'redux-vendors';
-            }
-            if (id.includes('react-router-dom')) {
-              return 'router-chunk';
-            }
-            if (id.includes('lightweight-charts')) {
-              return 'charts-chunk';
-            }
-            return 'vendor';
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 700, 
+    chunkSizeWarningLimit: 1100, 
   }
 });
